aae9c5449b3b853aeadf7582545c7377ff6fa945
r3dDoX <r3dDoX@users.noreply.github.com>
2023-11-07 14:01:13 +0100

    add border transformer

--- web-components/tokens/transformers/filterWeb.cjs
+++ web-components/tokens/transformers/filterWeb.cjs
@@ -1,3 +1,3 @@
-const acceptedTypes = ['color', 'dimension', 'font', 'number', 'custom-radius', 'custom-fontStyle', 'custom-shadow', 'custom-gradient']
+const acceptedTypes = ['color', 'dimension', 'font', 'number', 'custom-radius', 'custom-fontStyle', 'custom-shadow', 'custom-gradient', 'custom-stroke']
 
 module.exports = (token) => !token.type || acceptedTypes.includes(token.type)
