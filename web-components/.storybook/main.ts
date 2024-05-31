e298e0c38480ab0fd4644b6061eb3ba0f2eaf39b
r3dDoX <r3dDoX@users.noreply.github.com>
2024-05-31 22:50:21 +0200

    add proper typing

--- web-components/.storybook/main.ts
+++ web-components/.storybook/main.ts
@@ -1,16 +1,26 @@
 import { dirname, join } from 'path';
+import { mergeConfig } from 'vite';
+import turbosnap from 'vite-plugin-turbosnap';
+import { StorybookConfig } from '@storybook/web-components-vite';
 
-export default {
+const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-links'), getAbsolutePath('@storybook/addon-essentials')],
   framework: {
-    name: getAbsolutePath('@storybook/web-components-vite'),
+    name: '@storybook/web-components-vite',
     options: {},
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      plugins: configType === 'PRODUCTION' ? [turbosnap({ rootDir: config.root ?? process.cwd() })] : [],
+    });
   },
   docs: {
     autodocs: true,
   },
 };
+
+export default config;
 
 function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, 'package.json')));
