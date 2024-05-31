f83076cf8048ec7a4c55c04f0caa6e891e94ad88
r3dDoX <r3dDoX@users.noreply.github.com>
2024-05-31 14:14:46 +0200

    remove unnecessary merge config. Storybook uses default vite.config as of v7

--- web-components/.storybook/main.cjs
+++ web-components/.storybook/main.cjs
@@ -2,11 +2,6 @@ const {
   dirname,
   join,
 } = require('path');
-const {
-  mergeConfig,
-} = require('vite');
-const svg = require('vite-plugin-svgo');
-const turbosnap = require('vite-plugin-turbosnap');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -14,28 +9,6 @@ module.exports = {
   framework: {
     name: getAbsolutePath('@storybook/web-components-vite'),
     options: {},
-  },
-  async viteFinal(config, {configType}) {
-    return mergeConfig(config, {
-      plugins: [
-        ...(configType === 'PRODUCTION' ? [turbosnap({rootDir: config.root ?? process.cwd()})] : []),
-        svg({
-          plugins: [{
-            name: 'preset-default',
-            params: {
-              overrides: {
-                convertColors: {
-                  currentColor: true,
-                },
-                removeViewBox: false,
-              },
-            },
-          }, {
-            name: 'removeDimensions',
-          }],
-        }),
-      ],
-    });
   },
   docs: {
     autodocs: true,
